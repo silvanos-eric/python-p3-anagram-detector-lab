ff296ed928ba1600a748261d6c1000dbd3fca2dd
Silvanos Eric <ericoderito@gmail.com>
2024-09-14 17:04:59 +0300

    feat(anagram): add Anagram class with match and checker methods
    - Implemented Anagram class to handle anagram word matching.
    - Added `match` method to compare a list of words with the base word.
    - Added `checker` method to check if two words are anagrams.

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, word: str) -> None:
+        self.word = word
+
+    def match(self, word_list: list):
+        return [
+            possible_word for possible_word in word_list
+            if Anagram.checker(possible_word, self.word)
+        ]
+
+    @staticmethod
+    def checker(possible_word: str, word: str):
+        possible_word_sorted = sorted(possible_word)
+        word_sorted = sorted(word)
+        return True if possible_word_sorted == word_sorted else False
